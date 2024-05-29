2724174f9c59bc8b73dae3d2c356d461fb5dc884
Ashutosh Kumar Singh <ashutoshkumarsingh3946@gmail.com>
2024-05-29 12:46:08 +0530

    commit

--- redux_incre_decre/src/App.js
+++ redux_incre_decre/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import {bootstrap} from 'bootstrap'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
 export default function App() {
   return (
     <>
-    <div className='container'>
+      <h1 className='text-center'>Increment or Decrement </h1>
+      <h1 className='text-center'> Using Redux</h1>
+      <div className='container d-flex align-item-center justify-content-center'>
+        <div>
+          <button class="btn btn-outline-success bi bi-dash-lg m-2" type="submit"></button>
+        </div>
+        <div>
+          <h1>0</h1>
+        </div>
+        <div>
 
-    </div>
-
-    
-    
-     
-  
+          <button class="btn btn-outline-success bi bi-align-middle m-2" type="submit"></button>
+        </div>
+      </div>
 
     </>
   )
