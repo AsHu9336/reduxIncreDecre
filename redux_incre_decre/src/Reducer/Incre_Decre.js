722aceb01882fb2efe10f3ba95559addd225ceed
Ashutosh Kumar Singh <ashutoshkumarsingh3946@gmail.com>
2024-05-29 20:43:54 +0530

    commit

--- redux_incre_decre/src/Reducer/Incre_Decre.js
+++ redux_incre_decre/src/Reducer/Incre_Decre.js
@@ -1,12 +1,12 @@
 
-
-const ActionReducer = function (state = initialstate, action) {
+const initialstate = 0;
+export const ActionReducer = function (state = initialstate, action) {
     switch (action.type) {
         case Increment:
             return state = state + 1;
 
         case Decrement:
-            return state = state + 1;
+            return state = state - 1;
         default:
             return state;
     }
--- redux_incre_decre/src/Reducer/index.js
+++ redux_incre_decre/src/Reducer/index.js
@@ -1,3 +1,8 @@
-export const Index = function(){
 
-}
+import Incre_Decre from './Incre_Decre';
+import { combineReducers } from "redux";
+
+
+export const rootReducer = combineReducers({
+    Incre_Decre: Incre_Decre
+})
